705e5325b6df34f48f0c00898a87bf4efb2b7806
George <7jagjag@gmail.com>
2023-05-26 12:44:11 +0200

    :recycle: finish refacto

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,20 +1,18 @@
 import { clx } from "../utils/tailwind"
 
-const colorsByStatus = {
-  loading: 'blue',
-  standby: 'green',
-  failed: 'red',
-  fetching: 'teal',
-} as const
-
-type Status = keyof typeof colorsByStatus
-
+type Status = 'loading' | 'standby' | 'failed' | 'fetching'
 
 export default function Alert(props: { status: Status }) {
-  const color = colorsByStatus[props.status]
+  const className = `px-2 border rounded`
   
   return (
-    <span className={clx({ 'animate-pulse': props.status === 'loading'}, `capitalize rounded text-${color}-600 border border-${color}-400 bg-${color}-100 px-2`)}>
+    <span className={clx({ 
+      'animate-pulse': props.status === 'loading',
+      'text-green-600 border-green-400 bg-green-100' : props.status === 'standby',
+      'text-blue-600 border-blue-400 bg-blue-100' : props.status === 'loading',
+      'text-teal-600 border-teal-400 bg-teal-100' : props.status === 'fetching',
+      'text-red-600 border-red-400 bg-red-100' : props.status === 'failed',
+    }, className)}>
       {props.status}
     </span> 
   )
--- src/components/ShowMessages.tsx
+++ src/components/ShowMessages.tsx
@@ -2,21 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import { db } from "../utils/db"
 import { clx } from "../utils/tailwind"
 import Alert from "./Alert"
-
-const StatusBadgeButton = (props: { server: boolean, errored: null | VoidFunction }) => {
-  if (props.server) {
-    return <span className="bg-green-100 border border-green-400 rounded px-4 text-green-800">server</span>
-  }
-
-  if (typeof props.errored === 'function') {
-    return <button onClick={props.errored} className="bg-slate-100 hover:bg-slate-200 active:bg-slate-300 border border-slate-400 rounded px-4 text-slate-800">
-      resend?
-    </button>
-
-  }
-
-  return <span className="bg-orange-100 border border-orange-400 rounded px-4 text-orange-800 animate-pulse">local</span>
-}
+import StatusBadgeButton from "./StatusButton"
 
 const ShowMessages = () => {
   const { data, isLoading, isFetching, isError } = useQuery({
